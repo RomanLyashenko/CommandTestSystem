97d0abbbe70500b8ac6cf96a0a7e92460d29ae5a
sarkis234 <sarkis95283@gmail.com>
2025-01-10 22:52:30 +0300

    Added create test, class test, bag fix

--- Admin.cpp
+++ Admin.cpp
@@ -26,6 +26,43 @@ string Admin::getPass()
     return this->pass;
 }
 
+Test Admin::createTest()
+{
+    string category;
+    string name;
+    string question;
+    string answer;
+    int correctAnswer;
+    vector<string> questions;
+    int questionsNum;
+    vector<vector<string>> answersMatrix;
+    vector<int> correctAnswers;
+    cout << "Enter category: ";
+    cin >> category;
+    cout << "Enter test name: ";
+    cin >> name;
+    cout << "Enter amount of questions: ";
+    cin >> questionsNum;
+    cout << questions.size();
+    for (int i = 0; i < questionsNum; i++) {
+        cout << "Enter question " << i + 1 << ": ";
+        cin >> question;
+        questions.push_back(question);
+        vector<string> answers;
+       for (int j = 0; j < 3; j++) {
+            cout << "Enter answer " << j + 1 << ": ";
+            cin >> answer;
+            answers.push_back(answer);
+        }
+       answersMatrix.push_back(answers);
+        cout << "Enter correct answer: ";
+        cin >> correctAnswer;
+        correctAnswers.push_back(correctAnswer);
+    }
+    Test newTest(category, name, questions, answersMatrix, correctAnswers);
+    return newTest;
+}
+
 void Admin::changePass()
 {
     string newPass;
@@ -58,26 +95,35 @@ void Admin::changeLogin()
 
 void Admin::menuAdmin()
 {
+    int choise;
     for (int i = 0; i < 30; i++) {
         cout << "-";
     }
-    cout << endl << "Меню:" << endl;
+    cout << endl << "Menu:" << endl;
     for (int i = 0; i < 30; i++) {
         cout << "-";
     }
-    cout << endl << "1. Управление пользователями";
-    cout << endl << "2. Управление тестами:";
-    cout << endl << "3. Создание тестов";
-    cout << endl << "4. Изменение тестов";
-    cout << endl << "5. Удаление тестов";
-    cout << endl << "6. Импорт";
-    cout << endl << "7. Экспорт";
-    cout << endl << "8. Просмотр статистики";
+    cout << endl << "1. Create Test";
+    cout << endl << "2. Delete Test:";
+    cout << endl << "3. Add user";
+    cout << endl << "4. Delete user";
+    cout << endl << "5. Import test";
+    cout << endl << "6. Export test";
+    cout << endl << "7. Watch statistic";
+    cout << endl << "8. Change login";
+    cout << endl << "9. Change password";
     for (int i = 0; i < 30; i++) {
         cout << "-";
+    }
+    cin >> choise;
+    switch (choise) {
+    case 1:
+        createTest();
+        break;
+
     }
 }
-    
+
 Admin::~Admin()
 {
 }
