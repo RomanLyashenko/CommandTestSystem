b146bbbf233b45323bac02951e6c92185b090189
sarkis234 <sarkis95283@gmail.com>
2025-01-13 21:41:00 +0300

    Add export and import file, bug fix

--- Test.h
+++ Test.h
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 
-class Test{
+class Test {
 	string category;
 	string name;
 	vector<string> questions;
@@ -14,6 +14,9 @@ public:
 	Test(string category, string name, vector<string> questions, vector<vector<string>> answers, vector<int> correctAnswers);
 	string getCategory();
 	string getName();
+	vector<string> getQuestions();
+	vector<vector<string>> getAnswers();
+	vector<int> getCorrectAnswers();
 	void print();
 	~Test();
 };
