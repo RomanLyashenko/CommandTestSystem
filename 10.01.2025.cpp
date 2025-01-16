188feba4b08bf1972ad4dca6047f512cb8994615
AbsGosha <159914520+AbsGosha@users.noreply.github.com>
2025-01-16 17:52:05 +0300

    Add files via upload

---
+++ 10.01.2025.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include "User.h"
+#include "Test.h"
+#include "Admin.h"
+using namespace std;
+int main()
+{
+	vector<string>questions = { "1", "2", "3" };
+	vector<vector<string>> answers;
+	vector<int>correctAnswer = { 1, 2, 3 };
+	Test test1("math", "discret math", questions, answers, correctAnswer);
+	User user("math", "discret math");
+	user.goTest();
+	//string category;
+	//string name;
+	//cout << "Enter the test category: ";
+	//cin >> category
+	//cout << "Ente;r the test name: ";
+	//cout << endl;
+	//cin >> name;
+	//cout << endl;
+	
+}
